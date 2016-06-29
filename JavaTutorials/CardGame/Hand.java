d575b4fada4bf9717bb24713240033edae6b6ec1
Jayz07 <garyyjj@hotmail.com>
2016-06-29 11:46:29 +0800

    Update Hand.java

--- JavaTutorials/CardGame/Hand.java
+++ JavaTutorials/CardGame/Hand.java
@@ -1,31 +1,27 @@
-
 public class Hand{
-	public static void displayArray(int showArray[]){
-		for(int i:showArray){
-			System.out.println(i);
-		}
-		System.out.println();
+	
+	/**
+	 * This is a hand class which creates a hand. When creating this hand class it will create an array of empty cards just like
+	 * the deck class.
+	 * The empty card constructor is being used here because initially the cards in hand are all null.
+	 * Just like the deck class
+	 */
+	Card hand[] = new Card[53];
+	int selectedCard;
+	
+	public Hand(){
+		selectedCard = 0;
 	}
 	
-	public static void main(String args[]){
-		int test[] = new int[3];
-		int a,b;
-		
-		test[0] = 1;
-		test[1] = 2;
-		test[2] = 3;
-		
-		displayArray(test);
-		a = test[0];
-		b = test[2];
-		
-		test[0] = b;
-		test[2] = a;
-		
-		displayArray(test);
-		
-		
-		
+	public void getFirstCardPosition(){
+		if(hand[selectedCard] != null){
+			selectedCard++;
+		}
 	}
-
+	
+	public void addCard(Card c){
+		getFirstCardPosition();
+		hand[selectedCard] = c;
+	}
+	
 }
